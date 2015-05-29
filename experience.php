23ca93a845dde7de6df808101aa68e0afaeed6de
unknown <arn.alizee@gmail.com>
2015-05-29 10:36:52 +0200

    loader

--- experience.php
+++ experience.php
@@ -77,12 +77,101 @@
         .display{
             display: block;
         }
+
+        /* LOADER */
+        .fullLoader {
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            background: #262526;
+            z-index: 9999;
+        }
+        .loader {
+            font-size: 10px;
+            margin: 25em auto;
+            text-indent: -9999em;
+            width: 11em;
+            height: 11em;
+            border-radius: 50%;
+            background: #ffffff;
+            background: -moz-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+            background: -webkit-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+            background: -o-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+            background: -ms-linear-gradient(left, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+            background: linear-gradient(to right, #ffffff 10%, rgba(255, 255, 255, 0) 42%);
+            position: relative;
+            -webkit-animation: load3 1.4s infinite linear;
+            animation: load3 1.4s infinite linear;
+            -webkit-transform: translateZ(0);
+            -ms-transform: translateZ(0);
+            transform: translateZ(0);
+        }
+        .loader:before {
+            width: 50%;
+            height: 50%;
+            background: #FFF;
+            border-radius: 100% 0 0 0;
+            position: absolute;
+            top: 0;
+            left: 0;
+            content: '';
+        }
+        .loader:after {
+            background: #262526;
+            width: 75%;
+            height: 75%;
+            border-radius: 50%;
+            content: '';
+            margin: auto;
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+        }
+        @-webkit-keyframes load3 {
+            0% {
+                -webkit-transform: rotate(0deg);
+                transform: rotate(0deg);
+            }
+            100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg);
+            }
+        }
+        @keyframes load3 {
+            0% {
+                -webkit-transform: rotate(0deg);
+                transform: rotate(0deg);
+            }
+            100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg);
+            }
+        }
+        .txtLoading{
+            color: #fff;
+            font-size: 20px;
+            position: relative;
+            margin-top: -17%;
+        }
+        .logoChargement{
+            width: 250px;
+        }
+
     </style>
 </head>
 <body>
+<div class="fullLoader">
+    <div class="loader">Loading...</div>
+    <p class="txtLoading">Chargement de l'application</p>
+    <img src="images/logo.png" alt="" class="logoChargement "/>
+</div>
 
 <i class="fa fa-refresh icon-refresh" id="resetButton"></i>
+
 <div id="console"></div>
+
 <div id="containerObjet">
     <?php
     if(isset($_GET['experience'])){
@@ -129,13 +218,13 @@
     ?>
 
 </div>
+
 <div id="icon-env">
     <i class="fa fa-chevron-circle-left icon_env"></i>
     <i class="fa fa-chevron-circle-right icon_env"></i>
 </div>
 
-<div id="container">
-</div>
+<div id="container"></div>
 
 <script src="js/three.min.js"></script>
 <script src="js/ColladaLoader.js"></script>
@@ -222,7 +311,7 @@
             $(document).on('touchstart', onCanvasMouseDown);
             $("#resetButton").on('touchstart', resetBones);
             loadScreen(function(){
-                //C EST ICI !!!!!!!! LE LOADER
+                $('.fullLoader').hide();
             });
         });
 
