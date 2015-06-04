6b2745961998fe506ad5326873acf4f8f2c314c7
Jordan <jordandaita@free.fr>
2015-06-04 11:38:08 +0200

    encodage

--- includes/traitement.php
+++ includes/traitement.php
@@ -137,10 +137,10 @@ if(isset($_GET['updateExperience'])){
     $value = $_GET['value'];
     $id = $_GET['id'];
 
-    $requete = "UPDATE experience SET ".$element."='".addslashes($value)."' WHERE idExperience=".$id."";
+    $requete = "UPDATE experience SET ".$element."='".$value."' WHERE idExperience=".$id."";
     $base->query($requete);
 
-    $base->close();
+    $base->close();;
 
     unset($element);
     unset($value);
@@ -297,11 +297,12 @@ if(isset($_GET['downloadCsv'])){
 }
 
 function downloadCsv($idExperience, $nbProduit, $nomExperience, $base){
-    $entete = array("ID_PARTICIPANT", "NOM PARTICIPANT", "PRENOM PARTICIPANT", "DATE NAISSANCE", "GENRE AVATAR");
+    $entete = ['ID_PARTICIPANT', 'NOM PARTICIPANT', 'PRENOM PARTICIPANT', 'DATE NAISSANCE', 'GENRE AVATAR'];
     for($i=1; $i < $nbProduit+1 ; $i++){
         array_push($entete, 'ID_PRODUIT_'.$i, 'NOM_PRODUIT_'.$i, 'NOM_PRODUIT_'.$i, 'ANGLE_BGX_'.$i, 'ANGLE_BGZ_'.$i, 'ANGLE_BDX_'.$i, 'ANGLE_BDZ_'.$i, 'ANGLE_BUSTE_'.$i, 'DISTANCE_'.$i, 'ANGLE_BDX_'.$i);
     }
 
+//    echo $nomExperience;
     $file = fopen('../CSVFiles/'.$idExperience.'-'.$nomExperience.'.csv','w+');
 
     fputcsv($file, $entete); // Ligne de titres
@@ -335,13 +336,11 @@ function downloadZip($base){
     $requete = "SELECT * FROM experience";
     $resultats = $base->query($requete);
     $zip = new ZipArchive();
-
     while(($resultat = $resultats->fetch_array())){
-        $entete = array("ID_PARTICIPANT", "NOM PARTICIPANT", "PRENOM PARTICIPANT", "DATE NAISSANCE", "GENRE AVATAR");
+        $entete = ['ID_PARTICIPANT', 'NOM PARTICIPANT', 'PRENOM PARTICIPANT', 'DATE NAISSANCE', 'GENRE AVATAR'];
         $idExperience = $resultat['idExperience'];
         $nomExperience = $resultat['nom'];
         $nbProduit = $resultat['nbProduit'];
-
         downloadCsv($idExperience, $nbProduit, $nomExperience, $base);
 
         if($zip->open('../CSVFiles/Emolyse_experiences_'.date('d-m-Y').'.zip', ZipArchive::CREATE) === true) {
@@ -358,9 +357,6 @@ if(isset($_GET['suppressionFichier'])){
 
 function suppressionFichier()
 {
-    if (!file_exists('../CSVFiles')) {
-        mkdir('../CSVFiles', 0777, true);
-    }
     $repertoire = opendir('../CSVFiles');
     echo $repertoire;
     while(false !== ($fichier = readdir($repertoire)))
