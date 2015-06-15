d2b53719cea9151041da4d2933eb93c8cfdc9cfb
Rémy <drouet.remy@gmail.com>
2015-06-15 20:40:34 +0200

    MAJ Script de lancement (Emolyse.bat) Démarre en fenêtre agrandie et ferme la console

--- scripts/Emolyse.bat
+++ scripts/Emolyse.bat
@@ -1,1 +1,2 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -new-window "localhost/Emolyse"
+start C:\wamp\wampmanager.exe
+start /d "C:\Program Files (x86)\Google\Chrome\Application\" /MAX chrome.exe -new-window "localhost/Emolyse"
