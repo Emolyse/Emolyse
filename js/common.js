bd8cdaa0dc63997d0db40fe86e41bbf488bf92a5
Rémy <drouet.remy@gmail.com>
2015-08-18 19:35:09 +0200

    - Nettoyage script smooth scroll - correctif bug maj menu/nav

--- js/common.js
+++ js/common.js
@@ -5,15 +5,118 @@
 /**************************************
  *        SMOOTH SCROLLING SCRIPT     *
  *************************************/
+
+// SMOOTH SCROLL PARAMS
 var preventLRArrows = false;
 var preventScroll = false;
+var deltaError = 10;//Even after a scrollTo on an $anchor there is a small difference between body.scrollTop and anchor.offset.top this param solve this issue
+var smoothScrollDuration = 800;
 
-/***
- * Cette fonction se charge d'effectuer le smooth scrolling en fonction de l'ancre et elle met à jour les info relatives
- * au changement : menu, barre d'adresse, style
- * @param $anchor
- */
-function scrollTo($anchor){
+// SMOOTH SCROLL TOOLS
+function majScrollMenuStyle($anchor){
+    if ($anchor.data("invert") === true) {
+        $(".scroll-navbar").addClass("invert");
+    } else {
+        $(".scroll-navbar").removeClass("invert");
+    }
+}
+
+function getClosestScrollAnchor(){
+    var $res = $(".scroll-anchor").first();
+    $(".scroll-anchor").each(function () {
+        if ($("body").scrollTop()-$(this).offset().top>-deltaError){
+            $res = $(this);
+        } else {
+            return false;
+        }
+    });
+    return $res;
+}
+
+function majScrollMenuActive($anchor){
+    if($anchor.data("menu")){
+        var ref = "#"+$anchor.data("menu");
+    } else {
+        var ref = "#"+$anchor.attr("id");
+    }
+    history.replaceState(null, null, "http://localhost/Emolyse/"+ref);
+    $('.scroll-navbar li').removeClass('active');
+    $('.scroll-navbar li a[href^="'+ref+'"]').parent().addClass('active');
+}
+
+function getPrevScrollAnchor(){
+    var $res = $("body");
+    var currentScroll = $("body").scrollTop();
+    $(".scroll-anchor").each(function (index,e) {
+        if(currentScroll>$(this).offset().top+deltaError){
+            $res = $(this);
+        }else{
+            return false;
+        }
+    });
+    return $res;
+}
+
+function getNextScrollAnchor(){
+    var $res = $("body");
+    var currentScroll = $("body").scrollTop();
+    $(".scroll-anchor").each(function (index,e) {
+        if(currentScroll<$(this).offset().top-deltaError){
+            $res = $(this);
+            return false;
+        }
+    });
+    return $res;
+}
+
+// SMOOTH SCROLL HANDLERS
+function smoothScrollKeysHandler(e) {
+    //Pour le scroll vertical
+    if(e.keyCode>31 && e.keyCode<41)
+        e.preventDefault();
+    //top
+    if(e.keyCode == 38 && !preventScroll){
+        smoothScrollToPrev();
+    }
+    //bottom
+    if(e.keyCode == 40 && !preventScroll){
+        smoothScrollToNext();
+    }
+
+    //A supprimer quand pas de carousel
+    //left
+    if(e.keyCode == 37 && !preventLRArrows){
+        $('.carousel').carousel('prev');
+    }
+    //right
+    if(e.keyCode == 39 && !preventLRArrows){
+        $('.carousel').carousel('next');
+    }
+}
+
+function smoothScrollMouseWheelHandler(e){
+    e.preventDefault();
+    if(!preventScroll) {
+        if (e.originalEvent.wheelDelta >= 0) {
+            smoothScrollToPrev();
+        }
+        else {
+            smoothScrollToNext()
+        }
+    }
+}
+
+function smoothScrollClickHandler(e){
+    e.preventDefault();
+    var target = $(e.target).attr("href");
+    if(target==="#")
+        var $anchor = $(".scroll-anchor").first();
+    else var $anchor = $(""+target);
+    smoothScrollTo($anchor);
+}
+
+// SMOOTH SCROLL UTILITIES
+function smoothScrollTo($anchor){
     //ajouter le scroll watcher
     if(!preventScroll){
         preventScroll = true;
@@ -25,120 +128,55 @@ function scrollTo($anchor){
             var scroll = $anchor.offset().top;
 
             //on reccupere l'ID de l'ancre ou de son menu rattaché
-            if($anchor.data("menu")){
-                var ref = "#"+$anchor.data("menu");
-            } else {
-                var ref = "#"+$anchor.attr("id");
-            }
-            //on met a jour la navbar
-            history.replaceState(null, null, "http://localhost/Emolyse/"+ref);
-            $('.scroll-navbar li').removeClass('active');
-            $('.scroll-navbar li a[href^="'+ref+'"]').parent().addClass('active');
+            majScrollMenuActive($anchor);
         }
         $("body").animate({
             scrollTop:scroll
-        },1000, function () {
+        },smoothScrollDuration, function () {
             preventScroll = false;
-            //on vérifie que l'on a bien atteint l'autre section avant de changer le style du menu
-            //pour les ancre placées a la fin avec une hauteur < window.innerHeight
-            if (Math.abs($anchor.offset().top-$("body").scrollTop())<10){
-                if ($anchor.data("invert") === true) {
-                    $(".scroll-navbar").addClass("invert");
-                } else {
-                    $(".scroll-navbar").removeClass("invert");
-                }
+            //On situe le menu avant de mettre à jour son style (invert)
+            if (Math.abs($anchor.offset().top-$("body").scrollTop())>deltaError){
+                $anchor = getClosestScrollAnchor();
             }
+            majScrollMenuStyle($anchor);
         });
     }
 }
 
-function getPrevScrollAnchor(){
-    var $res = $("body");
-    var currentScroll = $("body").scrollTop();
-    $(".scroll-anchor").each(function (index,e) {
-        if(currentScroll>$(this).offset().top+10){
-            $res = $(this);
-        }else{
-            return false;
-        }
-    });
-    return $res;
-}
-function getNextScrollAnchor(){
-    var $res = $("body");
-    var currentScroll = $("body").scrollTop();
-    $(".scroll-anchor").each(function (index,e) {
-        if(currentScroll<$(this).offset().top-10){
-            $res = $(this);
-            return false;
-        }
-    });
-    return $res;
+function smoothScrollToPrev(){
+    smoothScrollTo(getPrevScrollAnchor());
 }
 
-function scrollToPrev(){
-    scrollTo(getPrevScrollAnchor());
-}
-function scrollToNext(){
-    scrollTo(getNextScrollAnchor());
-}
-function scrollKeysHandler(e) {
-    //Pour le scroll vertical
-    if(e.keyCode>31 && e.keyCode<41)
-        e.preventDefault();
-    //top
-    if(e.keyCode == 38 && !preventScroll){
-        scrollToPrev();
-    }
-    //bottom
-    if(e.keyCode == 40 && !preventScroll){
-        scrollToNext();
-    }
-
-    //Pour le carousel
-    //left
-    if(e.keyCode == 37 && !preventLRArrows){
-        $('.carousel').carousel('prev');
-    }
-    //right
-    if(e.keyCode == 39 && !preventLRArrows){
-        $('.carousel').carousel('next');
-    }
-}
-function mouseWheelHandler(e){
-    e.preventDefault();
-    if(!preventScroll) {
-        if (e.originalEvent.wheelDelta >= 0) {
-            scrollToPrev();
-        }
-        else {
-            scrollToNext()
-        }
-    }
+function smoothScrollToNext(){
+    smoothScrollTo(getNextScrollAnchor());
 }
 
-function scrollClickHandler(e){
-    e.preventDefault();
-    var target = $(e.target).attr("href");
-    if(target==="#")
-        var $anchor = $(".scroll-anchor").first();
-    else var $anchor = $(""+target);
-    console.log($anchor);
-    scrollTo($anchor);
-}
+function initSmoothScroll(duration){
+    //On met à jour le menu
+    if(duration)
+        smoothScrollDuration = duration;
+    var $anchor = getClosestScrollAnchor();
+    majScrollMenuStyle($anchor);
+    majScrollMenuActive($anchor);
 
-$(document).ready(function () {
-
-    $(document).keydown(scrollKeysHandler);
+    //On met en place tous les event handler
+    $(document).keydown(smoothScrollKeysHandler);
     var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
-    $(window).on(mousewheelevt,mouseWheelHandler);
-    $(".scroll-navbar a").click(scrollClickHandler);
+    $(window).on(mousewheelevt,smoothScrollMouseWheelHandler);
+    $(".scroll-navbar a").click(smoothScrollClickHandler);
+
+
+    //A supprimer quand pas de carousel
     $('#Home').on('slide.bs.carousel', function () {
         preventLRArrows = true;
     });
     $('#Home').on('slid.bs.carousel', function () {
         preventLRArrows = false;
     });
+}
+$(document).ready(function () {
+
+    initSmoothScroll();
 
     var $sprite = $('.sprite-pseudo');
 
